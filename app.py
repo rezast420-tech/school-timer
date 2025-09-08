0e85847bc6a4586c5d126f26952d70e59fa3bb33
rezast420-tech <rezast420@gmail.com>
2025-09-09 00:54:43 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-# صفحه اصلی با تایمر
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# صفحه درباره
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-# صفحه تایمر تعاملی
-@app.route("/timer")
-def timer():
-    return render_template("timer.html")
-
-# اجرای محلی برای تست
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run()
